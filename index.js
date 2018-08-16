a76d8116bea8c4a13ff82356f1b1f965181492a3
nisimotoryoga <42030915+nisimotoryoga@users.noreply.github.com>
2018-08-16 20:22:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var bot = controller.spawn({
 　　}
 
 controller.hears([''], ['direct_mention,mention'], function(bot, message) {
-　　　　bot.reply(message, 'hahaha');
+　　　　bot.reply(message, '経費のMVVを浸透させるためにやって来てん。 \n 詳しくはこれ見てな〜 \n https://mf.esa.io/posts/76277#%E7%B5%8C%E8%B2%BB%E6%9C%AC%E9%83%A8%E3%81%AEMVV');
+  
 　　});
 });
