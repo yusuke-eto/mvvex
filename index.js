511667f76ea872e37d7202856730842417a36d81
nisimotoryoga <42030915+nisimotoryoga@users.noreply.github.com>
2019-03-28 18:36:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ var bot = controller.spawn({
 controller.hears(['mvv'], ['ambient'], function(bot, message) {
 　　　　bot.reply(message, '経費のMVVを浸透させるためにやって来てん。 \n 詳しくはこれ見てな〜 \n https://mf.esa.io/posts/76277#%E7%B5%8C%E8%B2%BB%E6%9C%AC%E9%83%A8%E3%81%AEMVV');
 　　});
+  
+controller.hears(['mvv'], ['direct_mention'], function(bot, message) {
+　　　　bot.reply(message, 'githubはこれ \n https://github.com/nisimotoryoga/mvvex/edit/master/index.js \n\n 書き方はこのesaを参考にするとえーよ \n https://mf.esa.io/posts/93424');
+　　});
 
 controller.hears(['ZOOM'], ['direct_mention'], function(bot, message) {
 　　　　bot.reply(message, 'Zoom URL \n 堀内さん https://zoom.us/j/4367734105 \n 寺本さん https://zoom.us/j/7855344110 \n 北嶋さん https://zoom.us/j/7776860009 \n 住谷さん https://zoom.us/j/7475568036 \n 逆瀬川さん https://zoom.us/j/6020559468 \n 松本さん https://zoom.us/j/5661849566 \n 原さん https://zoom.us/j/8206768818 \n 横田さん https://zoom.us/j/4603468082 \n 後藤さん https://zoom.us/j/6820371657 \n 安中さん https://zoom.us/j/5730560839 \n 西本さん https://zoom.us/j/9076360566 \n 石塚さん https://zoom.us/j/2245365943 \n 柏崎さん https://zoom.us/j/7963331199 \n 伊藤大さん https://zoom.us/j/3532915271');
